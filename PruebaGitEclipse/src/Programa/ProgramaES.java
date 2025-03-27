521c4b8cb15c6ce093096edfc3660dd38c9dd81b
Sandra <sandranadalpastor@gmail.com>
2025-03-27 16:19:29 +0100

    He añadido un print contestando a Estela. Att: Sandra

--- PruebaGitEclipse/src/Programa/ProgramaES.java
+++ PruebaGitEclipse/src/Programa/ProgramaES.java
@@ -4,7 +4,7 @@ public class ProgramaES {
 
 	public static void main(String[] args) {
 		System.out.println("Hola mundo, soy Estela, como estáis?");
-
+		System.out.println("Holaa, soy Sandra");
 	}
 
 }
